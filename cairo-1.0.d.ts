9af1d57aefbbd03580b589ea0ffa481385194456
Nishant George Agrwal <nishantagrwal12993@gmail.com>
2016-01-06 22:54:19 +0530

    initial commit

---
+++ cairo-1.0.d.ts
@@ -0,0 +1,94 @@
+declare namespace imports.gi.cairo {
+
+class Context {
+
+
+}
+
+
+
+class Surface {
+
+
+}
+
+
+
+class Matrix {
+
+
+}
+
+
+
+class Pattern {
+
+
+}
+
+
+
+class Region {
+
+
+}
+
+
+
+class FontOptions {
+
+
+}
+
+
+
+class FontType {
+
+
+}
+
+
+
+class FontFace {
+
+
+}
+
+
+
+class ScaledFont {
+
+
+}
+
+
+
+class Path {
+
+
+}
+
+
+
+class RectangleInt {
+	public x: number;
+	public y: number;
+	public width: number;
+	public height: number;
+
+
+}
+
+
+
+enum Content {
+	COLOR = 4096,
+	ALPHA = 8192,
+	COLOR_ALPHA = 12288
+}
+
+
+
+function image_surface_create (): void;
+
+}
